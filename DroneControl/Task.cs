ac682d5742f6fff4e21f4ada5bf178c5e8b97452
Jacob Threadgould <jacob7395@gmail.com>
2019-03-31 19:36:36 +0100

    Started commenting the task file, also implemented multiple point serialize and deserilization for the GoTo Class.

--- DroneControl/Task.cs
+++ DroneControl/Task.cs
@@ -17,64 +17,117 @@ using VRageMath;
 
 namespace IngameScript.DroneControl.utility.task
 {
+
+    /// <summary>
+    /// A task object hold several actions and provides an interface to access the actions.
+    /// 
+    /// It also provides a serialization method to allow saving of tasks to strings.
+    /// </summary>
     public class Task
     {
         List<DroneAction> actions = new List<DroneAction>();
 
+        /// <summary>
+        /// Empty contructor
+        /// </summary>
         public Task() {}
 
+        /// <summary>
+        /// Get the next action if one is avalible else returns null.
+        /// </summary>
+        /// <returns>The next action</returns>
         public DroneAction Get_Next_Action()
         {
             DroneAction next_action = null;
+            // check if the is an action for this task
             if (this.actions.Count > 0)
                 next_action = this.actions[0];
 
             return next_action;
         }
 
+        /// <summary>
+        /// Called when the actions objectives have been met.
+        /// </summary>
+        /// <returns>True if actions are left</returns>
         public bool Action_Complete()
         {
-            this.actions.RemoveAt(0);
+            if (this.actions.Count > 0)
+                this.actions.RemoveAt(0);
 
             return this.actions.Count > 0;
         }
 
+        /// <summary>
+        /// Add an action to the task que
+        /// </summary>
+        /// <param name="action"></param>
         public void Add_Action(DroneAction action)
         {
             actions.Add(action);
         }
 
+        /// <summary>
+        /// Attempts to deserialize an action then add it to the que
+        /// </summary>
+        /// <param name="action"></param>
         public void Add_Action(String action)
         {
             
         }
     }
 
+    /// <summary>
+    /// Type used to indecate what the current action is
+    /// </summary>
     public enum action_tpye
     {
         GoTo
     }
 
+    /// <summary>
+    /// All action are stored as there abstract class this alows for a generic interface, when action are used they must
+    /// be converted to the correct action class.
+    /// </summary>
     public abstract class DroneAction
     {
+        /// <summary>
+        /// Used to determin what action this represents.
+        /// </summary>
+        /// <returns>An enumeration form action_type</returns>
         abstract public action_tpye get_type();
 
+        /// <summary>
+        /// Method to convert the actions into a string
+        /// </summary>
+        /// <returns></returns>
         abstract public string Serialize();
-    }
 
+        /// <summary>
+        /// Given a string this method will attemt to fill the objects properts with the data provided.
+        /// </summary>
+        /// <param name="objective"></param>
+        abstract public bool Deserialization(string objective);
+    }
+    
+    /// <summary>
+    /// The GoTo action holds the infomation requiered to navigate to a point.
+    /// 
+    /// String format - "GoTo:x,y,z
+    /// </summary>
     public class GoTo : DroneAction
     {
         Vector3D target;
+
+        // this allows the drone to be off from the target by a set amount.
+        // this is not currently serealizable
         float tolorance = 5;
 
         List<Vector3D> route;
 
-        public const action_tpye type = action_tpye.GoTo;
-
 
         public GoTo(Vector3D target, float tolorance = 5)
         {
-            this.target = target;
             this.tolorance = tolorance;
             this.route = new List<Vector3D>();
             this.route.Add(target);
@@ -82,8 +135,7 @@ namespace IngameScript.DroneControl.utility.task
 
         public GoTo(string objective)
         {
-            Vector3D target = this.Deserialization(objective);
-            this.target = target;
+            this.Deserialization(objective);
             this.route = new List<Vector3D>();
             this.route.Add(target);
         }
@@ -108,26 +160,48 @@ namespace IngameScript.DroneControl.utility.task
 
         public override string Serialize()
         {
-            return String.Format("GoTo:{0},{1},{2}", this.target.X, this.target.Y, this.target.Z);
+            string actiong_name = "GoTo:";
+
+            // add each point to the output, points are added so the last point is first in the string
+            string points = "";
+            foreach (Vector3D point in this.route)
+            {
+                // format the string with a devider
+                points += String.Format("{0},{1},{2}:", point.X, point.Y, point.Z);
+            }
+            // remove the extra devided added
+            points.Remove(points.Length - 1);
+
+            // return the name and the points
+            return actiong_name + points;
         }
 
-        public Vector3D Deserialization(string objective)
+        public override bool Deserialization(string objective)
         {
-            System.Text.RegularExpressions.Regex type_check = new System.Text.RegularExpressions.Regex("^GoTo:([^,]+),([^,]+),([^,]+)");
+            System.Text.RegularExpressions.Regex type_check = new System.Text.RegularExpressions.Regex("^GoTo:");
             System.Text.RegularExpressions.Match correct_type = type_check.Match(objective);
 
-            Vector3D out_val = Vector3D.NegativeInfinity;
+            bool success = false;
 
             if (correct_type.Success)
             {
-                float x = float.Parse(correct_type.Groups[1].Value);
-                float y = float.Parse(correct_type.Groups[2].Value);
-                float z = float.Parse(correct_type.Groups[3].Value);
+                this.route = new List<Vector3D>();
 
-                out_val = new Vector3D(x, y, z);
+                string[] split_objective = objective.Split(':');
+
+                for (int i = 1; i < split_objective.Length; i++)
+                {
+                    float x = float.Parse(split_objective[0]);
+                    float y = float.Parse(split_objective[1]);
+                    float z = float.Parse(split_objective[2]);
+
+                    this.route.Add(new Vector3D(x, y, z));
+
+                    success = true;
+                }
             }
 
-            return out_val;
+            return success;
         }
 
         public override action_tpye get_type()
